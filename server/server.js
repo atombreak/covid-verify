d7696fdb1ea16ce4ff44270a1f6a5c74a4e476f5
atombreak <trustchochaatie2@gmail.com>
2021-12-29 15:56:25 +0200

    test'

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@
 
 const path = require('path');
 const express = require("express");
-// const json = require('json-server');
+const json = require('json-server');
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname,'..','build')));
-// app.use('/api', json.router(path.join(__dirname,'..','db.json')));
+app.use('/api', json.router(path.join(__dirname,'..','db.json')));
 
 // console.log(path.join(__dirname,'..','db.json'));
 
