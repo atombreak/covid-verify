ac63b3edec1ce8407b26f95a4138f163f74afa5e
atombreak <trustchochaatie2@gmail.com>
2021-12-29 15:44:30 +0200

    test'

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname,'..','build')));
-app.use('/api', json.router(path.join(__dirname,'..','db.json')));
+// app.use('/api', json.router(path.join(__dirname,'..','db.json')));
 
 // console.log(path.join(__dirname,'..','db.json'));
 
