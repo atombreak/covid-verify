1dd95a3430338021475ddcb67fe5467534990410
atombreak <trustchochaatie2@gmail.com>
2021-12-29 15:19:56 +0200

    test'

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@
 
 const path = require('path');
 const express = require("express");
+const json = require('json-server');
 
 const app = express();
 const port = 9000 || process.env.PORT
 
 app.use(express.static(path.join(__dirname,'..','build')));
+app.use('/api', json.router(path.join(__dirname,'..','db.json')));
+
+console.log(path.join(__dirname,'..','db.json'));
 
 app.all('*', (req,res) => {
     res.sendFile(path.join(__dirname,'..',"build/index.html"));
